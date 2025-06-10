0bcceb0e3b76be630a9e5b035c8eae403a72f867
dicky ganda <dickyganda@gmail.com>
2025-06-10 14:29:35 +0700

    create auth login and registration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use Hash;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class AuthController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')->withSuccess('You have Successfully loggedin');
+        }
+
+        return redirect('/')->withError('Oppes! You have entered invalid credentials');
+    }
+
+    public function postRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        $user = $this->create($data);
+
+        // Auth::login($user); 
+
+        return redirect()->route('auth.login')->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard.index');
+        }
+
+        return redirect('/')->withSuccess('Opps! You do not have access');
+    }
+
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('/products');
+    return view('/auth/login');
 });
 
 //route resource for products
 Route::resource('/products', ProductController::class);
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('dashboard', [AuthController::class, 'dashboard']); 
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
